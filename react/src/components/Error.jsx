e6aaccc58f5d1cf4fb5e18dcf26332dfbaa40335
Daniel DeGroff <daniel@inversoft.com>
2017-02-14 21:21:27 -0700

    working

--- react/src/components/Error.jsx
+++ react/src/components/Error.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { browserHistory, Link } from 'react-router';
 
-
 class Error extends Component {
-  
   render() {
     return (
        <div className="error">

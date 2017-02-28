72b09b87475fcb7c68a3cd49d7c85f15d3236c70
Daniel DeGroff <daniel@inversoft.com>
2017-02-28 10:32:48 -0700

    Working

--- react/config/config.js
+++ react/config/config.js
@@ -3,35 +3,37 @@
 var request = require('request');
 const configFile = require ('./config.json');
 
-var configuration, callBack;
+var configuration;
 
 // Retrieve the Passport Configuration from our back end application.
 var production = process.env['NODE_ENV'] === 'production';
 var baseURL = production ? configFile.production.todo.url : configFile.development.todo.url;
-request.get(baseURL + '/api/passport/config', function(err, res, body) {
-  if (!err && res.statusCode === 200) {
-    if (production) {
-      configFile.production.passport = JSON.parse(body);
-      configuration = configFile.production;
+function retrieveConfiguration(callBack) {
+  request.get(baseURL + '/api/passport/config', function(err, res, body) {
+    if (!err && res.statusCode === 200) {
+      if (production) {
+        configFile.production.passport = JSON.parse(body);
+        configuration = configFile.production;
+      } else {
+        configFile.development.passport = JSON.parse(body);
+        configuration = configFile.development;
+      }
+
+      if (callBack) {
+        callBack(configuration);
+      }
     } else {
-      configFile.development.passport = JSON.parse(body);
-      configuration = configFile.development;
+      console.error(err);
+      throw new Error('Unable to retrieve the Passport configuration. [' + res.statusCode + ']');
     }
+  });
+}
 
-    if (callBack) {
-      callBack(configuration);
-    }
-  } else {
-    console.error(err);
-    throw new Error('Unable to retrieve the Passport configuration. [' + res.statusCode + ']');
-  }
-});
-
-module.exports = function(providedCallBack) {
+module.exports = function(callBack) {
   if (typeof configuration === 'undefined') {
-    callBack = providedCallBack;
+    retrieveConfiguration(callBack);
   } else {
-    providedCallBack(configuration);
+    callBack(configuration);
   }
 };
 
